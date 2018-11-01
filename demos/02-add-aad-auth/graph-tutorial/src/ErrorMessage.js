1983a5ec6f27d7049be74c8cb9d6513e5cafe935
Jason Johnston <jasonjoh@users.noreply.github.com>
2018-11-01 13:46:45 -0400

    Copied app to next folder

---
+++ demos/02-add-aad-auth/graph-tutorial/src/ErrorMessage.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Alert } from 'reactstrap';
+
+export default class ErrorMessage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.message = props.message;
+    this.debug = props.debug;
+  }
+
+  render() {
+    let debug = null;
+    if (this.debug) {
+      debug = <pre class="alert-pre border bg-light p-2"><code>{this.debug}</code></pre>;
+    }
+    return (
+      <Alert color="danger">
+        <p class="mb-3">{this.message}</p>
+        {debug}
+      </Alert>
+    );
+  }
+}
