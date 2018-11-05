14065e9d860e5a06481ae6341c389d162d5ff46d
Jason Johnston <jasonjoh@users.noreply.github.com>
2018-11-05 10:00:07 -0500

    Copied previous project to new folder

---
+++ demos/03-add-msgraph/graph-tutorial/src/App.js
@@ -0,0 +1,124 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Container } from 'reactstrap';
+import { UserAgentApplication } from 'msal';
+import NavBar from './NavBar';
+import ErrorMessage from './ErrorMessage';
+import Welcome from './Welcome';
+import config from './Config';
+import { getUserDetails } from './GraphService';
+import 'bootstrap/dist/css/bootstrap.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.userAgentApplication = new UserAgentApplication(config.appId, null, null);
+
+    var user = this.userAgentApplication.getUser();
+
+    this.state = {
+      isAuthenticated: (user !== null),
+      user: {},
+      error: null
+    };
+
+    if (user) {
+      // Enhance user object with data from Graph
+      this.getUserProfile();
+    }
+  }
+
+  render() {
+    let error = null;
+    if (this.state.error) {
+      error = <ErrorMessage message={this.state.error.message} debug={this.state.error.debug} />;
+    }
+
+    return (
+      <Router>
+        <div>
+          <NavBar
+            isAuthenticated={this.state.isAuthenticated}
+            authButtonMethod={this.state.isAuthenticated ? this.logout.bind(this) : this.login.bind(this)}
+            user={this.state.user}/>
+          <Container>
+            {error}
+            <Route exact path="/"
+              render={(props) =>
+                <Welcome {...props}
+                  isAuthenticated={this.state.isAuthenticated}
+                  user={this.state.user}
+                  authButtonMethod={this.login.bind(this)} />
+              } />
+          </Container>
+        </div>
+      </Router>
+    );
+  }
+
+  async login() {
+    try {
+      await this.userAgentApplication.loginPopup(config.scopes);
+      await this.getUserProfile();
+    }
+    catch(err) {
+      var errParts = err.split('|');
+      this.setState({
+        isAuthenticated: false,
+        user: {},
+        error: { message: errParts[1], debug: errParts[0] }
+      });
+    }
+  }
+
+  logout() {
+    this.userAgentApplication.logout();
+  }
+
+  async getUserProfile() {
+    try {
+      // Get the access token silently
+      // If the cache contains a non-expired token, this function
+      // will just return the cached token. Otherwise, it will
+      // make a request to the Azure OAuth endpoint to get a token
+
+      var accessToken = await this.userAgentApplication.acquireTokenSilent(config.scopes);
+
+      if (accessToken) {
+        // Get the user's profile from Graph
+        var user = await getUserDetails(accessToken);
+        this.setState({
+          isAuthenticated: true,
+          user: {
+            displayName: user.displayName,
+            email: user.mail || user.userPrincipalName
+          },
+          error: null
+        });
+      }
+    }
+    catch(err) {
+      var error = {};
+      if (typeof(err) === 'string') {
+        var errParts = err.split('|');
+        error = errParts.length > 1 ?
+          { message: errParts[1], debug: errParts[0] } :
+          { message: err };
+      } else {
+        error = {
+          message: err.message,
+          debug: JSON.stringify(err)
+        };
+      }
+
+      this.setState({
+        isAuthenticated: false,
+        user: {},
+        error: error
+      });
+    }
+  }
+}
+
+export default App;
---
+++ demos/03-add-msgraph/graph-tutorial/src/ErrorMessage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Alert } from 'reactstrap';
+
+export default class ErrorMessage extends React.Component {
+  render() {
+    let debug = null;
+    if (this.props.debug) {
+      debug = <pre className="alert-pre border bg-light p-2"><code>{this.props.debug}</code></pre>;
+    }
+    return (
+      <Alert color="danger">
+        <p className="mb-3">{this.props.message}</p>
+        {debug}
+      </Alert>
+    );
+  }
+}
