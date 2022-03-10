7587e2fe27a09958395dc2f88137cb14ef41c9e0
Jason Johnston <jasonjoh@microsoft.com>
2022-03-10 11:33:24 -0500

    Fixed test so it runs

--- demo/graph-tutorial/src/App.test.tsx
+++ demo/graph-tutorial/src/App.test.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { PublicClientApplication } from '@azure/msal-browser';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const pca = new PublicClientApplication({
+  auth: {
+    clientId: ''
+  }
+});
+
+jest.spyOn(pca, 'addEventCallback').mockImplementation((fn) => {
+  return '';
+})
+
+test('renders welcome page', () => {
+  render(<App pca = {pca} />);
+  const titleElement = screen.getByRole('heading');
+  expect(titleElement).toBeInTheDocument();
+  expect(titleElement).toHaveTextContent(/React Graph Tutorial/i);
 });
