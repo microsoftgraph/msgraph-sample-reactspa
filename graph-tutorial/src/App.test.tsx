4a72e5ee4d3f15ca97f330897f53cc2b436acd4d
Jason Johnston <jasonjoh@users.noreply.github.com>
2022-09-12 13:04:12 -0400

    Fix for tests

--- graph-tutorial/src/App.test.tsx
+++ graph-tutorial/src/App.test.tsx
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
